dca546a4b7dc02f008b8578ac5ca5ee7172dc8af
pushp1502 <pushpanjali201818@gmail.com>
2024-05-30 00:06:15 +0530

    Updated the files with some new learning

---
+++ programming/Oops/univers.java
@@ -0,0 +1,13 @@
+class abc {
+	public static int area(int 1, int b){
+		return 1*b;
+	}
+	public static int volume(int 1, int b, int h){
+		return 1*b*h;
+	}
+	public static void main(string[] args){
+		System.out.printIn("hello world");
+		System.out.printIn("Area= " +area(5,4));
+		System.out.printIn("Volume= "+volume(5,6,7));
+	}
+}
