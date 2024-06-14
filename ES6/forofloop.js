ebfcd390a917d1cc5f9f415fbbb6a9e48023c99c
pushp1502 <pushpanjali201818@gmail.com>
2024-06-14 17:42:16 +0530

    learnt how to use for/of loop

---
+++ ES6/forofloop.js
@@ -0,0 +1,15 @@
+// the for/of loop
+
+let arr = [1,2,3,4,5];
+
+//for(i = 0; i < arr.length; i++){
+for(num of arr){
+    console.log(num);
+}
+
+let str = "IAMLEARNING";
+
+for(let c of str){
+    console.log(c);
+
+}
