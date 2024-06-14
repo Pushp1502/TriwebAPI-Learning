a59d1c349b9d63243a7205867e1c38d20f09dbd7
pushp1502 <pushpanjali201818@gmail.com>
2024-06-15 00:57:41 +0530

    Practicing new codes as symbols in javascript

--- ES6/symbols.js
+++ ES6/symbols.js
@@ -1,18 +1,28 @@
-const person = {
-    id: 1234,
-    pname: "opaque",
-};
+// const person = {
+//     id: 1234,
+//     pname: "opaque",
+// };
 
-const opaque = Symbol('opaque');
-person[opaque] = "Hidden data";
+// const opaque = Symbol('opaque');
+// person[opaque] = "Hidden data";
 
-// console.log(person);
-// person.id = 2;
-// console.log(person);
-// console.log(Object.getOwnPropertyNames(person));
+// // console.log(person);
+// // person.id = 2;
+// // console.log(person);
+// // console.log(Object.getOwnPropertyNames(person));
 
-console.log(person[opaque]);
+// console.log(person[opaque]);
 
-const IamHardWorking = Symbol('opaque');
-console.log(person[IamHardWorking]);
+// const IamHardWorking = Symbol('opaque');
+// console.log(person[IamHardWorking]);
+
+// global symbol
+
+const xyz = symbol.for('xyz');
+person[xyz] = "Not hidden symbol data";
+console.log(person[xyz]);
+
+const IknowThis = symbol.for('xyz');
+console.log(person[IknowThis]);
+
 
