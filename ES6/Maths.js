776138bca56b7249d780cb6ba2fe1f0ca74cb901
pushp1502 <pushpanjali201818@gmail.com>
2024-06-16 23:21:26 +0530

    Learning about the Maths and Numbers method in javascript

---
+++ ES6/Maths.js
@@ -0,0 +1,23 @@
+// Math.trunc();
+
+// console.log(Math.trunc(5.7));
+// console.log(Math.trunc(5.4));
+// console.log(Math.trunc(-5.3));
+// console.log(Math.trunc(5));
+
+//Math.sign()
+// console.log(Math.sign(-19));
+// console.log(Math.sign(20));
+// console.log(Math.sign(0));
+
+//Math.cbrt()
+// console.log(Math.cbrt(27));
+
+//Math.sqrt()
+// console.log(Math.sqrt(64));
+
+//Math.log2() Math.log10()
+
+console.log(Math.log10(10));
+console.log(Math.log2(2));
+
---
+++ ES6/number.js
@@ -0,0 +1,27 @@
+// EPSILON
+
+//let x = Number.EPSILON;
+//console.log(x);
+
+//MIN_SAFE_INTEGER
+
+// let y = Number.MAX_SAFE_INTEGER;
+//console.log(y);
+
+//Max_SAFE_INTEGER
+
+//let z = Number.MIN_SAFE_INTEGER;
+//console.log(z);
+
+//Number method
+
+let a= 123;
+let b = 9000345678;
+console.log(Number.isInteger(a));
+
+console.log("Safe integer",Number.isSafeInteger(b));
+
+
+
+
+
