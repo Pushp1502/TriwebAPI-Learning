362312c54125b9f40f006b77420cf80a2a692a3b
pushp1502 <pushpanjali201818@gmail.com>
2024-06-23 23:50:14 +0530

    Practice the crud operation in my sql

---
+++ MySQL/Table.sql
@@ -0,0 +1,2 @@
+CREATE TABLE students ( ID INT(11) NOT NULL, Name VARCHAR NOT NULL, Course VARCHAR NOT NULL, Company VARCHAR(20), PRIMARY KEY (ID));
+/* Create table with primary key and with details */
